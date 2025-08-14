07b7f8d5292bcc3ee36f476954e645c156de180f
Joyeta30 <joyeta2002s@gmail.com>
2025-08-14 10:39:39 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // adjust to your folder
+  theme: {
+     extend: {
+      fontFamily: {
+        quicksand: ['Quicksand', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
